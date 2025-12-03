e2f6ddd9f725cab5f5f3894743f1e5cf7d6695c0
soda-xyz <sodezan@gmail.com>
2025-12-03 18:55:33 +0100

    In main until login works post rework

--- docker/nginx/entrypoint.sh
+++ docker/nginx/entrypoint.sh
@@ -7,13 +7,14 @@ TARGET="/etc/nginx/nginx.conf"
 # Provide safe defaults if environment variables are missing
 : "${BACKEND_HOST:=slowfall-backend.azurewebsites.net}"
 : "${BACKEND_PORT:=8080}"
+: "${FRONTEND_HOST:=slowfall-frontend.azurewebsites.net}"
 : "${CLIENT_MAX_BODY_SIZE:=10m}"
 : "${PROXY_READ_TIMEOUT:=90s}"
 : "${PROXY_SEND_TIMEOUT:=90s}"
 
 if [ -f "$TEMPLATE" ]; then
   echo "Generating $TARGET from template"
-  envsubst '\$BACKEND_HOST \$BACKEND_PORT \$CLIENT_MAX_BODY_SIZE \$PROXY_READ_TIMEOUT \$PROXY_SEND_TIMEOUT' < "$TEMPLATE" > "$TARGET"
+  envsubst '\$BACKEND_HOST \$BACKEND_PORT \$FRONTEND_HOST \$CLIENT_MAX_BODY_SIZE \$PROXY_READ_TIMEOUT \$PROXY_SEND_TIMEOUT' < "$TEMPLATE" > "$TARGET"
 fi
 
 exec nginx -g 'daemon off;'
